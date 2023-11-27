85917d22149c705e10bad1fb28a8e1b1e5239b90
김우빈 <kwb020312@naver.com>
2023-11-27 21:34:29 +0900

    Feat: CTA & keyboard Events

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="cta">
+      <p className="cta-text">맘에 드는 프로젝트가 있나요?</p>
+      <br className="sm:block hidden" />
+      다음엔 같이 만들어봐요!
+      <Link to={"/contact"} className="btn">
+        연락하기
+      </Link>
+    </section>
+  );
 };
 
 export default CTA;
